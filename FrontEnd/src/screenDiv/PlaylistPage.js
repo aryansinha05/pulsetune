cdc286931e76057edd5ba2d12659b1af88ba2bcf
aryansiha05 <sinhaaryan026@gmail.com>
2025-07-18 15:58:20 +0530

    no playlist text

--- FrontEnd/src/screenDiv/PlaylistPage.js
+++ FrontEnd/src/screenDiv/PlaylistPage.js
@@ -6,7 +6,10 @@ const PlaylistPage = ({ user, setUser }) => {
     <div className="p-4 sm:p-8 mt-4 sm:mt-8">
       <h1 className="text-2xl sm:text-3xl font-bold mb-4 sm:mb-6 text-white">Your Playlists</h1>
       {user.playlists.length === 0 ? (
+        <>
         <p className='ml-1 text-gray-400'>No Playlist Exists!</p>
+        <p className='ml-1 text-gray-400'>Creat a Playlist to add music of your favourite genre.</p>
+        </>
       ) : (
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 gap-4 sm:gap-8">
           {user.playlists.map((playlist) => (
